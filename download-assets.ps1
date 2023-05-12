e1268729063699a485955946dad97a479a5e099b
Archit Bajpai <archit9169@gmail.com>
2023-05-12 14:38:42 +0530

    Update download-assets.ps1

--- download-assets.ps1
+++ download-assets.ps1
@@ -1,24 +1,36 @@
+# Constant Vars
+$GITHUB_API_LINK = "https://api.github.com/repos/{0}/releases/latest"
+$GITHUB_HEADERS = @{
+    "Accept" = "application/vnd.github.v3+json"
+}
+
+# Function
 function Download-Asset($asset) {
-    $repoName = $asset.repo
-    $uri = "https://api.github.com/repos/$repoName/releases/latest"
-    $headers = @{
-        "Accept" = "application/vnd.github.v3+json"
+    
+    # Get release and asset details from api
+    $latestRelease = Invoke-RestMethod -Uri ($GITHUB_API_LINK -f $asset.repo) -Headers $GITHUB_HEADERS
+    $latestAssets = $latestRelease.assets | Select-Object -Property "name", @{
+        Name = "url"; Expression = { "browser_download_url" };
     }
-
-    $latestRelease = Invoke-RestMethod -Uri $uri -Headers $headers
-    $assetName = $asset.name
-    $assetDownloadUrl = $latestRelease.assets | Where-Object { $_.name -like "$assetName*" } | Select-Object -First 1 -ExpandProperty browser_download_url
-    $assetVersion = $latestRelease.tag_name
-    $assetNameWithVersion = $assetName + "-" + $assetVersion + $assetDownloadUrl.Substring($assetDownloadUrl.LastIndexOf("."))
-    $downloadPath = Join-Path -Path "source" -ChildPath $assetNameWithVersion
-
-    if (Test-Path -Path $downloadPath) {
-        Write-Host "Using cached asset $assetNameWithVersion"
-        return
+    
+    # Check each asset in the list
+    foreach ($name in $asset.name) {
+        
+        # Get asset details from list
+        $item = $latestAssets | Where-Object {
+            $PSItem.name -like "*$name*"
+        } | Select-Object -First 1
+        if ($item -eq $null) { continue }
+        
+        # Download or use Cached
+        $dlPath = Join-Path -Path "source" -ChildPath $item.name
+        if (Test-Path -Path $dlPath) {
+            Write-Host "Using cached asset $($item.name)"
+            continue
+        }
+        Write-Host "Downloading asset $($item.name)"
+        Invoke-WebRequest -Uri $item.url -OutFile $dlPath
     }
-
-    Write-Host "Downloading asset $assetNameWithVersion"
-    Invoke-WebRequest -Uri $assetDownloadUrl -OutFile $downloadPath
 }
 
 $assets = Get-Content "assets.yml" | ConvertFrom-Yaml
